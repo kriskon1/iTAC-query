c4ce3dea8aebf570ed6500364fc7329158f96d87
kriskon1 <126876298+kriskon1@users.noreply.github.com>
2025-07-02 15:39:39 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,10 @@
-USERNAME = "KRISKON"
-PASSWORD = "PASSWORD"
-SERVER_URL = "http://ves1-itacv2-02.vnet.valeo.com:8080/mes/imsapi/rest/actions"
+USERNAME = "..."
+PASSWORD = "..."
+SERVER_URL = "...../imsapi/rest/actions"
 
-stationnumber = "VES1-GEN601-130-01"
-# serialnumbers = ["C0640124345D47E0"]
-serialnumbers = ["C0640123273C098A"]
+stationnumber = "xx-yy-zz"
+serialnumbers = None
 
-# with open("serial.txt", mode="r") as file:
-#    serialnumbers = [line.strip() for line in file]
-#    # serialnumbers = [line.strip() for _, line in zip(range(10), file)]
-#    print(f"Serial number reading done, total serial numbers: {len(serialnumbers)}")
+with open("serial.txt", mode="r") as file:
+   serialnumbers = [line.strip() for line in file]
+   print(f"Serial number reading done, total serial numbers: {len(serialnumbers)}")
